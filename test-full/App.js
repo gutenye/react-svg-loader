dc4d6e4823a05d2fc1041f83f523a93d33c7eb24
Guten Ye <ywzhaifei@gmail.com>
2017-03-08 19:43:48 +0800

    publish

--- test-full/App.js
+++ test-full/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import IconDummy from './dummy.svg'
+import IconProducts from './products.svg'
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <IconDummy button gray id='id'/>
+        <IconProducts />
       </div>
     )
   }
